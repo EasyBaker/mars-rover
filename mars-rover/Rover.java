92a9d851531b7dcba72c7cd6a3e764ee5f24eed2
TrumpNat1on <31477682+TrumpNat1on@users.noreply.github.com>
2018-09-17 12:58:45 -0400

    Add files via upload

--- mars-rover/Rover.java
+++ mars-rover/Rover.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Rover extends Rover
+public class Rover
 {
     // instance variables
     private double x;
@@ -38,6 +38,7 @@ public class Rover extends Rover
         this.numPics = 0;
         this.name = name;
         this.health = 10;
+        this.energy = 10;
         this.isAlive = true;
     }
 
@@ -49,6 +50,7 @@ public class Rover extends Rover
         this.dir = dir;
         this.name = name;
         this.health = 10;
+        this.energy = 10;
         this.isAlive = true;
     }
     
@@ -102,9 +104,11 @@ public class Rover extends Rover
            }
            energy -= 1;
            System.out.println(this.name + " moved forward");
-       } else {
+       } else if (isAlive == false) {
            System.out.println(this.name + " can't move. It's ded.");
-       }    
+       } else if (energy == 0) {
+           System.out.println(this.name + " can't move. It has no energy.");
+        }
     }
     
     public void moveTo(int x, int y)
@@ -163,12 +167,15 @@ public class Rover extends Rover
             if (memory < 10) {
                 numPics += 1;
                 memory += 1;
+                this.energy -= 1;
                 System.out.println(this.name + " took a picture at [" + this.x + ", " + this.y + "] facing " + this.direction);
             } else {
                 System.out.println(this.name + " cannot take a picture, it has no spare memory.");
             }
-        } else {
+        } else if (isAlive == false) {
             System.out.println(this.name + " cannot take a picture, it's ded.");
+        } else if (energy == 0) {
+            System.out.println(this.name + " can't move. It has no energy.");
         }
     }
     
@@ -176,9 +183,12 @@ public class Rover extends Rover
         if (isAlive && energy > 0) {
             numPics = 0;
             memory = 0;
+            this.energy -= 1;
             System.out.println(this.name + " transmitted its pictures back to earth.");
-        } else {
-            System.out.println(this.name + " cannot transmit pictures, it's ded.");
+        } else if (isAlive == false) {
+            System.out.println(this.name + " cannot take a picture, it's ded.");
+        } else if (energy == 0) {
+            System.out.println(this.name + " can't move. It has no energy.");
         }
     }
     
@@ -194,12 +204,17 @@ public class Rover extends Rover
         if (isAlive && energy > 0) {
             if (other.isAlive) {
                 health -= damage;
+                this.energy -= 1;
                 System.out.println(this.name + " attacks " + other.name + " and does " + damage + " damage.");
-            } else {
+            } else if (isAlive == false) {
                 System.out.println(other.name + " cannot be attacked, because it's ded.");
+            } else if (energy == 0) {
+                System.out.println(other.name + " cannot be attacked, because it's out of energy.");
+            } else if (isAlive == false) {
+                System.out.println(this.name + " cannot attack " + other.name + " because it's ded.");
+            } else if (energy == 0) {
+                System.out.println(this.name + " cannot attack " + other.name + " because it has no energy.");
             }
-        } else {
-            System.out.println(this.name + " cannot attack " + other.name + " because it's ded.");
         }
     }
     
@@ -207,17 +222,24 @@ public class Rover extends Rover
         System.out.println(this.name + " shoots " + other.name + " with space lasers.");
         System.out.println(other.name + " goes 'beep beep aaaaaaakkkkkk!' and dies");
         
+        this.energy -= 1;
         other.isAlive = false;
     }
     
     public void revive(Rover other) {
         System.out.println(this.name + " revives " + other.name + " with magik.");
         System.out.println(other.name + " goes 'beep bop eeeeeeeeekkkkkk!' and comes back to life");
+        this.energy -= 1;
+    }
+    
+    public String getName() {
+        return name;
     }
     
     public String toString() {
         getDirectionName();
         return "Rover[name=" + name + ",x=" + x + ",y=" + y + 
-               ",dir=" + direction + ",isAlive=" + isAlive + "]";
+               ",dir=" + direction + ",isAlive=" + isAlive + ",energy=" + energy + 
+               "]";
     }
 }
--- mars-rover/Runner.java
+++ mars-rover/Runner.java
@@ -1,46 +1,119 @@
-
 /**
- * Write a description of class Runner here.
- *
- * @author (your name)
+ * Write a description of class RoverRunner here.
+ * 
+ * @author (your name) 
  * @version (a version number or a date)
  */
-public class Runner extends Runner
+
+import java.util.Scanner;
+
+public class Runner
 {
     public static void main(String[] args) {
+        // Make a SimpleScanner
+        SimpleScanner input = new SimpleScanner();
+        
+        // Make some Rovers
         Rover r1 = new Rover("Hitler");
         Rover r2 = new Rover("Stalin", 10, -4, 2);
-        System.out.println(r1.toString());
-        r1.charge(3);
-        r1.rotate(1);
-        r1.move(5);
-        System.out.println(r1);
-        r1.takePicture();
-        r1.transmitPictures();
-        r1.battle(r2);
-        ///r2.rotate(1);
-        ///System.out.println(r2);
-        ///r1.move();
-        ///System.out.println(r1);
-        ///r2.rotate(1);
-        ///System.out.println(r2);
-        ///r2.rotate(-1);
-        ///System.out.println(r2);
-        ///r1.rotate(3);
-        ///System.out.println(r1);
         
-        ///r1.kill(r2);
-        ///System.out.println(r1);
-        ///System.out.println(r2);
+        // Make a RoverGroup and add Rovers
+        RoverGroup group = new RoverGroup();
+        group.add(r1);
+        group.add(r2);
         
-        ///r1.revive(r2);
-        ///System.out.println(r1);
-        ///System.out.println(r2);
+        // Now let's do stuff
+        boolean running = true;
+        String exitCommand = "quit";
         
-        ///r1.move();        
-        ///System.out.println(r1);
+        while (running) {
+            // Input name
+            System.out.print("Enter the name of the Rover to act: ");
+            String name = input.readString();
+            
+            // Select Rover with matching name
+            Rover actor = group.find(name);
+            
+            if (actor != null) {
+                // If the rover is found
+                System.out.print("Enter a command: ");
+                String command = input.readString();
+                
+                if (command.equals("move")) {
+                    System.out.print("Enter distance to move: ");
+                    int distance = input.readInt();
+                    actor.move(distance);
+                }
+                else if (command.equals("rotate")) {
+                    System.out.print("Enter (-1, -7) to rotate left or (1, 7) to rotate right: ");
+                    double rotation = input.readInt();
+                    actor.rotate(rotation);
+                }
+                else if (command.equals("kill")) {
+                    System.out.print("Enter the name the target rover: ");
+                    String targetName = input.readString();
+                    
+                    Rover target = group.find(targetName);
+                    
+                    if (target != null) {
+                        actor.kill(target);
+                        System.out.println(target);
+                    }
+                    else {
+                        System.out.println("Error: No such target.");
+                    }
+                }
+                else if (command.equals("battle")) {
+                    System.out.print("Enter the name the target rover: ");
+                    String targetName = input.readString();
+                    
+                    Rover target = group.find(targetName);
+                    
+                    if (target != null) {
+                        actor.battle(target);
+                        System.out.println(target);
+                    }
+                    else {
+                        System.out.println("Error: No such target.");
+                    }
+                }
+                else if (command.equals("revive")) {
+                    System.out.print("Enter the name the target rover: ");
+                    String targetName = input.readString();
+                    
+                    Rover target = group.find(targetName);
+                    
+                    if (target != null) {
+                        actor.revive(target);
+                        System.out.println(target);
+                    }
+                    else {
+                        System.out.println("Error: No such target.");
+                    }
+                }
+                else if (command.equals("take picture")) {
+                    actor.takePicture();
+                }
+                else if (command.equals("transmit picture")) {
+                    actor.transmitPictures();
+                }
+                else {
+                    System.out.println("Error: Invalid command.");
+                }
+                
+                System.out.println(actor);
+            }
+            else if (name.equals(exitCommand)) {
+                running = false; // set flag to exit loop
+            }
+            else {
+                System.out.println("Error: " + name + " doesn't exist.");
+            }
+            
+            // just a blank line
+            System.out.println();
+        }
         
-        ///r1.teleport(5, 10);
-        ///System.out.println(r1);
+        System.out.println("Goodbye.");
     }
 }
